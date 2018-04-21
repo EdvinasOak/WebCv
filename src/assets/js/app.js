bef7d0a894166d86e74ee5b6848e07f6106b8efb
Edvinas Aukstakalnis <ed@Edvinass-MacBook-Pro.local>
2018-04-21 13:55:33 +0100

    Fixed Javascript Issue

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,64 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
+
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+	 //Fade animation for each section
+	 var controller = new ScrollMagic.Controller();
+
+	   $('.animation').each(function(){		 
+		// create a scene	 
+ 		var ourScene = new ScrollMagic.Scene({
+		triggerElement: this,
+		//duration: '85%',
+		reverse:false,
+		trigerHook:1
+	  }).setClassToggle(this, 'fade-in').addTo(controller);
+
+	   });
+
+	   //hero animation
+	   TweenMax.to(".centered", 2, {y:-280, ease:Back.easeOut});
+
+
+	   //Skills animation
+		var ctrl = new ScrollMagic.Controller();
+
+		$('.service').each(function(){
+
+			var serviceStager = TweenMax.staggerFrom('.stagger', 0.5, {opacity:0, y:200, delay:0.5},0.2);
+			
+		   new ScrollMagic.Scene({
+			   triggerElement: this,
+			   reverse:false
+		   })
+		   .setTween(serviceStager)
+		   .addTo(ctrl);
+
+		})
+
+		//Contact animmation
+		$('.contact').each(function(){
+			
+						var serviceStager2 = TweenMax.staggerFrom('.stagger2', 0.5, {opacity:0, y:200, delay:0.5},0.2);
+						
+					   new ScrollMagic.Scene({
+						   triggerElement: this,
+						   reverse:false
+					   })
+					   .setTween(serviceStager2)
+					   .addTo(ctrl);
+			
+					})
+			
