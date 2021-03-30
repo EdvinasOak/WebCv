a7846a0852b073252ec9ab7017728e24154bb19b
Edvinas Aukstakalnis <ed@Edvinass-MacBook-Air.local>
2021-03-30 20:59:06 +0100

    upgrade gulp sass version

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,73 +1,72 @@
-import $ from 'jquery';
-import whatInput from 'what-input';
+import $ from "jquery";
 
 window.$ = $;
 
-import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-
 $(document).foundation();
 
-
-
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+	document.getElementById("myNav").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+	document.getElementById("myNav").style.width = "0%";
 }
 
+//Fade animation for each section
+var controller = new ScrollMagic.Controller();
 
-	 //Fade animation for each section
-	 var controller = new ScrollMagic.Controller();
-
-	   $('.animation').each(function(){		 
-		// create a scene	 
- 		var ourScene = new ScrollMagic.Scene({
+$(".animation").each(function () {
+	// create a scene
+	var ourScene = new ScrollMagic.Scene({
 		triggerElement: this,
 		//duration: '85%',
-		reverse:false,
-		trigerHook:1
-	  }).setClassToggle(this, 'fade-in').addTo(controller);
+		reverse: false,
+		trigerHook: 1,
+	})
+		.setClassToggle(this, "fade-in")
+		.addTo(controller);
+});
 
-	   });
+//hero animation
+TweenMax.to(".centered", 2, { y: -280, ease: Back.easeOut });
 
-	   //hero animation
-	   TweenMax.to(".centered", 2, {y:-280, ease:Back.easeOut});
+//Skills animation
+var ctrl = new ScrollMagic.Controller();
 
+$(".service").each(function () {
+	var serviceStager = TweenMax.staggerFrom(
+		".stagger",
+		0.5,
+		{ opacity: 0, y: 200, delay: 0.5 },
+		0.2
+	);
 
-	   //Skills animation
-		var ctrl = new ScrollMagic.Controller();
+	new ScrollMagic.Scene({
+		triggerElement: this,
+		reverse: false,
+	})
+		.setTween(serviceStager)
+		.addTo(ctrl);
+});
 
-		$('.service').each(function(){
+//Contact animmation
+$(".contact").each(function () {
+	var serviceStager2 = TweenMax.staggerFrom(
+		".stagger2",
+		0.5,
+		{ opacity: 0, y: 200, delay: 0.5 },
+		0.2
+	);
 
-			var serviceStager = TweenMax.staggerFrom('.stagger', 0.5, {opacity:0, y:200, delay:0.5},0.2);
-			
-		   new ScrollMagic.Scene({
-			   triggerElement: this,
-			   reverse:false
-		   })
-		   .setTween(serviceStager)
-		   .addTo(ctrl);
-
-		})
-
-		//Contact animmation
-		$('.contact').each(function(){
-			
-						var serviceStager2 = TweenMax.staggerFrom('.stagger2', 0.5, {opacity:0, y:200, delay:0.5},0.2);
-						
-					   new ScrollMagic.Scene({
-						   triggerElement: this,
-						   reverse:false
-					   })
-					   .setTween(serviceStager2)
-					   .addTo(ctrl);
-			
-					})
-			
+	new ScrollMagic.Scene({
+		triggerElement: this,
+		reverse: false,
+	})
+		.setTween(serviceStager2)
+		.addTo(ctrl);
+});
